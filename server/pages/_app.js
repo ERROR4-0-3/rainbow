534a922336e1e5c0fb92e04528fb4eafc63188ef
ERROR403 <46407695+ERROR4-0-3@users.noreply.github.com>
2022-05-23 19:23:06 +0200

    upload the entire build

---
+++ server/pages/_app.js
@@ -0,0 +1,95 @@
+"use strict";
+(() => {
+var exports = {};
+exports.id = 888;
+exports.ids = [888];
+exports.modules = {
+
+/***/ 484:
+/***/ ((module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.a(module, async (__webpack_handle_async_dependencies__, __webpack_async_result__) => { try {
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(997);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _rainbow_me_rainbowkit__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(921);
+/* harmony import */ var wagmi__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(906);
+/* harmony import */ var wagmi__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(wagmi__WEBPACK_IMPORTED_MODULE_2__);
+var __webpack_async_dependencies__ = __webpack_handle_async_dependencies__([_rainbow_me_rainbowkit__WEBPACK_IMPORTED_MODULE_1__]);
+_rainbow_me_rainbowkit__WEBPACK_IMPORTED_MODULE_1__ = (__webpack_async_dependencies__.then ? (await __webpack_async_dependencies__)() : __webpack_async_dependencies__)[0];
+
+
+
+
+
+const { chains , provider  } = (0,_rainbow_me_rainbowkit__WEBPACK_IMPORTED_MODULE_1__.configureChains)([
+    wagmi__WEBPACK_IMPORTED_MODULE_2__.chain.mainnet,
+    wagmi__WEBPACK_IMPORTED_MODULE_2__.chain.polygon,
+    wagmi__WEBPACK_IMPORTED_MODULE_2__.chain.optimism,
+    wagmi__WEBPACK_IMPORTED_MODULE_2__.chain.arbitrum
+], [
+    _rainbow_me_rainbowkit__WEBPACK_IMPORTED_MODULE_1__.apiProvider.alchemy(process.env.ALCHEMY_ID),
+    _rainbow_me_rainbowkit__WEBPACK_IMPORTED_MODULE_1__.apiProvider.fallback()
+]);
+const { connectors  } = (0,_rainbow_me_rainbowkit__WEBPACK_IMPORTED_MODULE_1__.getDefaultWallets)({
+    appName: "Eyad Dev App",
+    chains
+});
+const wagmiClient = (0,wagmi__WEBPACK_IMPORTED_MODULE_2__.createClient)({
+    autoConnect: true,
+    connectors,
+    provider
+});
+function MyApp({ Component , pageProps  }) {
+    return /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(wagmi__WEBPACK_IMPORTED_MODULE_2__.WagmiProvider, {
+        client: wagmiClient,
+        children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(_rainbow_me_rainbowkit__WEBPACK_IMPORTED_MODULE_1__.RainbowKitProvider, {
+            chains: chains,
+            children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(Component, {
+                ...pageProps
+            })
+        })
+    });
+}
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (MyApp);
+
+__webpack_async_result__();
+} catch(e) { __webpack_async_result__(e); } });
+
+/***/ }),
+
+/***/ 997:
+/***/ ((module) => {
+
+module.exports = require("react/jsx-runtime");
+
+/***/ }),
+
+/***/ 906:
+/***/ ((module) => {
+
+module.exports = require("wagmi");
+
+/***/ }),
+
+/***/ 921:
+/***/ ((module) => {
+
+module.exports = import("@rainbow-me/rainbowkit");;
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__(484));
+module.exports = __webpack_exports__;
+
+})();
